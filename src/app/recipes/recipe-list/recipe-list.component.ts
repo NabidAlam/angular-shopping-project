b9e4a45e3db5b9fc6b70f87e4a3f892544c341f5
NabidAlam <msa.nabid007@gmail.com>
2019-04-01 19:02:03 +0600

    event binding nav

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,11 +8,17 @@ import { Recipe } from '../recipe.model'
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [
-    new Recipe('A test','this is a test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test','this is a test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test','this is a test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test','this is a test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test','this is a test desc', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg')
+// tslint:disable-next-line: max-line-length
+    new Recipe('Pizza', 'Spicy Meatloaf with Fiesta Rice', 'https://st2.depositphotos.com/1192512/7019/v/950/depositphotos_70194861-stock-illustration-chocolate-chips-cookie-vector-illustration.jpg'),
+// tslint:disable-next-line: max-line-length
+    new Recipe('Burger', 'Spicy Meatloaf with Fiesta Rice', 'https://i.pinimg.com/originals/15/18/25/151825f3c9267d4ce22f28c00789a6f0.jpg'),
+    new Recipe('Pastry', 'Spicy Meatloaf with Fiesta Rice', 'https://www.splide.com/wp-content/uploads/2011/08/paptert.jpg'),
+// tslint:disable-next-line: max-line-length
+    new Recipe('Cheese Burger', 'Spicy Meatloaf with Fiesta Rice', 'https://sparkpeo.hs.llnwd.net/e1/resize/630m620/e4/nw/2/5/l258791879.jpg'),
+// tslint:disable-next-line: max-line-length
+    new Recipe('Cake', 'Spicy Meatloaf with Fiesta Rice', 'https://www.readyseteat.com/sites/g/files/qyyrlu501/files/uploadedImages/img_4250_1456.JPEG')
+
+
   ];
   constructor() { }
 
